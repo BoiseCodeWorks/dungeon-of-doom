af3093a2b2aeb4f91d1889d89da2734eeabd1964
javaday <javaday@gmail.com>
2017-06-28 18:12:07 -0600

    Created the IUserInterface interface and concrete class

---
+++ BCW.ConsoleGame/BCW.ConsoleGame/User/ConsoleInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCW.ConsoleGame.User
+{
+    public class ConsoleInterface : IUserInterface
+    {
+        public void Clear()
+        {
+            Console.Clear();
+        }
+
+        public void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        public string GetInput(string prompt)
+        {
+            Console.Write(prompt);
+
+            return Console.ReadLine();
+        }
+    }
+}
